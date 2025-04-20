2b887f4f644f0c545e14c94da9ba3ca6986679a7
Machrus T. <9brinristek9@gmail.com>
2025-04-20 12:32:10 +0700

    tes

--- src/lib/components/ButtonJoin.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  let { buttonText, phoneNumber, defaultMessage } = $props();  
-</script>
-
-<div class="btn-box">
-  <a href={`https://wa.me/${phoneNumber}?text=${defaultMessage}`} class="theme-btn btn-one">
-    <span class="text">{buttonText}</span><i class="icon-3"></i>
-    </a>
-</div>
--- src/lib/stores.svelte.ts
+++
@@ -1,3 +0,0 @@
-export const currentMenuActive = $state({
-	current: '',
-});
