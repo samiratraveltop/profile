b533078a0216102b397eb6cf717149f653509153
samiratraveltop <samiratraveltop@users.noreply.github.com>
2025-04-20 05:16:42 +0000

    deploy: ebfab1be06c2ad38ebd3ec9bc717889c04fe19ff

---
+++ _app/immutable/chunks/BJH52Ax8.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_16hrf8e)==null?void 0:s.base)??"/samiratravel";var a;const t=((a=globalThis.__sveltekit_16hrf8e)==null?void 0:a.assets)??e;export{t as a,e as b};
