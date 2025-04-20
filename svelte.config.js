9eb40c295f8229c3a0480d2abbaca1839f95f4ff
Machrus T. <9brinristek9@gmail.com>
2025-04-20 12:37:42 +0700

    prod

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/samiratravel' : '',
+			base: process.env.NODE_ENV === 'production' ? '/profile' : '',
 		},
 	}
 };
